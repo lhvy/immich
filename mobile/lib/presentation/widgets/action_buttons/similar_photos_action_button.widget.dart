4474b484415aa7b987910dcdbd33bd01fb6448f7
Adam Gastineau <adam@agg.im>
2026-07-31 10:22:25 -0700

    chore(mobile): more complicated Freezed pass

--- mobile/lib/presentation/widgets/action_buttons/similar_photos_action_button.widget.dart
+++ mobile/lib/presentation/widgets/action_buttons/similar_photos_action_button.widget.dart
@@ -35,7 +35,7 @@ class SimilarPhotosActionButton extends ConsumerWidget {
           location: SearchLocationFilter(),
           camera: SearchCameraFilter(),
           date: SearchDateFilter(),
-          display: SearchDisplayFilters(isNotInAlbum: false, isArchive: false, isFavorite: false),
+          display: const SearchDisplayFilters(isNotInAlbum: false, isArchive: false, isFavorite: false),
           rating: SearchRatingFilter(),
           mediaType: AssetType.other,
         ),
